88c7c6bc826f23641ad3a9e07ce3c3c94b52b03c
Sai-S-Mc <sai.satti97@gmail.com>
2025-03-02 14:40:02 -0700

    Created basic project structure - initial commit

---
+++ src/Component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Component(){
+    return(
+        <div className="Component">
+            <h2>Hello from Component</h2>
+        </div>
+    )
+}
