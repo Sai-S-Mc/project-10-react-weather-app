2285ba6b122d878ae337512728f6322f815ae831
Sai-S-Mc <sai.satti97@gmail.com>
2025-03-04 14:16:43 -0700

    Created a Footer component , and added GitHub and Netlify links

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This weather project was coded by{" "}
+        <a href="https://github.com/Sai-S-Mc" target="_blank" rel="noreferrer">
+          Sai Satti
+        </a>
+        , is open-sourced on{" "}
+        <a
+          href="https://github.com/Sai-S-Mc/project-10-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        , and is hosted on{" "}
+        <a
+          href="https://project-10-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
