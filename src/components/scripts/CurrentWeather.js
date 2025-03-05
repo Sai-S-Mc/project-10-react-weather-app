732a0c2192254ef6208570bbd910e345f6a28cc8
Sai-S-Mc <sai.satti97@gmail.com>
2025-03-05 12:05:36 -0700

    Added current weather section and styled it

--- src/components/scripts/CurrentWeather.js
+++ src/components/scripts/CurrentWeather.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "../styles/CurrentWeather.css";
 
 export default function CurrentWeather() {
   return (
     <div className="CurrentWeather">
-      <div>
-        <div className="temperature-wrapper">🌥️9°C|F</div>
-        <div>Feels like : 0°C</div>
-        <div>Toronto, Canada</div>
+      <div className="left-float">
+        <div className="temperature-wrapper">
+          <span className="icon">🌥️</span>{" "}
+          <span className="temperature">9</span>{" "}
+          <span className="unit">°C | F</span>
+        </div>
+        <div className="feels-like">Feels like : 0°C</div>
+        <div className="location">Toronto, Canada</div>
       </div>
       <div>
         <ul>
