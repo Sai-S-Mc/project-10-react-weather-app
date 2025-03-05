544f66ff8a9e095cab8077c112fbb020f209e4f0
Sai-S-Mc <sai.satti97@gmail.com>
2025-03-04 22:06:45 -0700

    Added JSX to CurrentWeather.js

--- src/components/scripts/CurrentWeather.js
+++ src/components/scripts/CurrentWeather.js
@@ -3,7 +3,20 @@ import React from "react";
 export default function CurrentWeather() {
   return (
     <div className="CurrentWeather">
-      <p>Current Weather</p>
+      <div>
+        <div className="temperature-wrapper">🌥️9°C|F</div>
+        <div>Feels like : 0°C</div>
+        <div>Toronto, Canada</div>
+      </div>
+      <div>
+        <ul>
+          <li>Mostly Cloudy</li>
+          <li>Humidity : 20%</li>
+          <li>Wind: 5 km/h</li>
+          <li>Today's Low: -1°C</li>
+          <li>Today's High: 10°C</li>
+        </ul>
+      </div>
     </div>
   );
 }
