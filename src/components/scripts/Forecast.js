c6de616cb0bafc3efa4b51c8cab59c0c599b8594
Sai-S-Mc <sai.satti97@gmail.com>
2025-03-06 15:37:38 -0700

    Updated the forecast using bootstrap grid on Forecast.js

--- src/components/scripts/Forecast.js
+++ src/components/scripts/Forecast.js
@@ -1,88 +1,101 @@
 import React from "react";
 import "../styles/Forecast.css";
 import ReactAnimatedWeather from "react-animated-weather";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Forecast() {
   return (
     <div className="Forecast">
-      <div className="daily-forecast">
-        Mon
-        <br />
-        <br />
-        <ReactAnimatedWeather
-          icon={"PARTLY_CLOUDY_DAY"}
-          color={"#6090E7"}
-          size={50}
-          animate={true}
-        />
-        <br />
-        <br />
-        <div className="daily-high-low">
-          <strong>13° </strong>2°
+      <div className="row">
+        <div className="col-md">
+          <div className="daily-forecast">
+            Mon
+            <br />
+            <br />
+            <ReactAnimatedWeather
+              icon={"PARTLY_CLOUDY_DAY"}
+              color={"#6090E7"}
+              size={50}
+              animate={true}
+            />
+            <br />
+            <br />
+            <div className="daily-high-low">
+              <strong>13° </strong>2°
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="daily-forecast">
-        Mon
-        <br />
-        <br />
-        <ReactAnimatedWeather
-          icon={"PARTLY_CLOUDY_DAY"}
-          color={"#6090E7"}
-          size={50}
-          animate={true}
-        />
-        <br />
-        <br />
-        <div className="daily-high-low">
-          <strong>13° </strong>2°
+        <div className="col-md">
+          <div className="daily-forecast">
+            Mon
+            <br />
+            <br />
+            <ReactAnimatedWeather
+              icon={"PARTLY_CLOUDY_DAY"}
+              color={"#6090E7"}
+              size={50}
+              animate={true}
+            />
+            <br />
+            <br />
+            <div className="daily-high-low">
+              <strong>13° </strong>2°
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="daily-forecast">
-        Mon
-        <br />
-        <br />
-        <ReactAnimatedWeather
-          icon={"PARTLY_CLOUDY_DAY"}
-          color={"#6090E7"}
-          size={50}
-          animate={true}
-        />
-        <br />
-        <br />
-        <div className="daily-high-low">
-          <strong>13° </strong>2°
+        <div className="col-md">
+          <div className="daily-forecast">
+            Mon
+            <br />
+            <br />
+            <ReactAnimatedWeather
+              icon={"PARTLY_CLOUDY_DAY"}
+              color={"#6090E7"}
+              size={50}
+              animate={true}
+            />
+            <br />
+            <br />
+            <div className="daily-high-low">
+              <strong>13° </strong>2°
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="daily-forecast">
-        Mon
-        <br />
-        <br />
-        <ReactAnimatedWeather
-          icon={"PARTLY_CLOUDY_DAY"}
-          color={"#6090E7"}
-          size={50}
-          animate={true}
-        />
-        <br />
-        <br />
-        <div className="daily-high-low">
-          <strong>13° </strong>2°
+        <div className="col-md">
+          <div className="daily-forecast">
+            Mon
+            <br />
+            <br />
+            <ReactAnimatedWeather
+              icon={"PARTLY_CLOUDY_DAY"}
+              color={"#6090E7"}
+              size={50}
+              animate={true}
+            />
+            <br />
+            <br />
+            <div className="daily-high-low">
+              <strong>13° </strong>2°
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="daily-forecast">
-        Mon
-        <br />
-        <br />
-        <ReactAnimatedWeather
-          icon={"PARTLY_CLOUDY_DAY"}
-          color={"#6090E7"}
-          size={50}
-          animate={true}
-        />
-        <br />
-        <br />
-        <div className="daily-high-low">
-          <strong>13° </strong>2°
+        <div className="col-md">
+          <div className="daily-forecast">
+            Mon
+            <br />
+            <br />
+            <ReactAnimatedWeather
+              icon={"PARTLY_CLOUDY_DAY"}
+              color={"#6090E7"}
+              size={50}
+              animate={true}
+            />
+            <br />
+            <br />
+            <div className="daily-high-low">
+              <strong>13° </strong>2°
+            </div>
+          </div>
         </div>
       </div>
     </div>
