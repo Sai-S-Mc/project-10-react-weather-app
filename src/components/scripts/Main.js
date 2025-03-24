8078d6e01692fdee367b8823702e8ab064bee429
Sai-S-Mc <sai.satti97@gmail.com>
2025-03-24 12:54:12 -0400

    Main.js - added search functionality

--- src/components/scripts/Main.js
+++ src/components/scripts/Main.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Main.css";
 import CurrentWeather from "./CurrentWeather";
 import Forecast from "./Forecast";
 import axios from "axios";
 
 export default function Main({ weatherData, handleApiResponse }) {
+  const [city, setCity] = useState("Iqaluit");
+
+  function search() {
+    let apiKey = "tbfob32e017e01391b34fe15b81ad2a6";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleApiResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function captureUserInput(event) {
+    let userInput = event.target.value;
+    setCity(userInput);
+  }
+
   let form = (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="row">
         <div className="col-sm-10 ps-1 pe-1">
           <input
             type="search"
             placeholder="Enter a city name"
             className="search-box"
+            onChange={captureUserInput}
           />
         </div>
         <div className="col-sm-2 ps-1 pe-1">
@@ -33,11 +52,7 @@ export default function Main({ weatherData, handleApiResponse }) {
       </div>
     );
   } else {
-    let apiKey = "tbfob32e017e01391b34fe15b81ad2a6";
-    let city = "London";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleApiResponse);
-
+    search();
     return (
       <div className="Main">
         {form}
