ae9f006644fd2946edcd51d78b9d7587d1671e87
Sai-S-Mc <sai.satti97@gmail.com>
2025-03-27 12:02:06 -0400

    Condensed code using conditional operators

--- src/components/scripts/CurrentWeather.js
+++ src/components/scripts/CurrentWeather.js
@@ -16,116 +16,77 @@ export default function CurrentWeather({ weather }) {
     setUnit("metric");
   }
 
-  if (unit === "metric") {
-    return (
-      <div className="CurrentWeather">
-        <div className="left-float">
-          <div className="temperature-wrapper">
-            <span className="icon">
-              <CurrentWeatherIcon iconName={weather.icon} />
-            </span>{" "}
-            <span className="temperature">
-              {Math.round(weather.temperature)}
-            </span>{" "}
-            <span className="unit">
-              °C | °
-              <a href="/" onClick={showFahrenheit}>
-                F
-              </a>
+  let celsiusTag = (
+    <span className="unit">
+      °C | °
+      <a href="/" onClick={showFahrenheit}>
+        F
+      </a>
+    </span>
+  );
+
+  let fahrenheitTag = (
+    <span className="unit">
+      °
+      <a href="/" onClick={showCelsius}>
+        C
+      </a>{" "}
+      | °F
+    </span>
+  );
+
+  return (
+    <div className="CurrentWeather">
+      <div className="left-float">
+        <div className="temperature-wrapper">
+          <span className="icon">
+            <CurrentWeatherIcon iconName={weather.icon} />
+          </span>{" "}
+          <span className="temperature">
+            {unit === "metric"
+              ? Math.round(weather.temperature)
+              : Math.round((weather.temperature * 9) / 5 + 32)}
+          </span>{" "}
+          {unit === "metric" ? celsiusTag : fahrenheitTag}
+        </div>
+        <div className="text-center">
+          <div>
+            Feels like :{" "}
+            <span className="bluish-grey-text">
+              {unit === "metric"
+                ? Math.round(weather.realFeel) + " °C"
+                : Math.round((weather.realFeel * 9) / 5 + 32) + " °F"}
             </span>
           </div>
-          <div className="text-center">
-            <div>
-              Feels like :{" "}
-              <span className="bluish-grey-text">
-                {Math.round(weather.realFeel)} °C
-              </span>
-            </div>
-            <div className="mt-3">{weather.country}</div>
-          </div>
-          <div className="text-center location">{weather.city}</div>
+          <div className="mt-3">{weather.country}</div>
         </div>
-        <div>
-          <ul className="mb-0 ps-1rem">
-            <li className="text-capitalize">{weather.description}</li>
-            <li>
-              Humidity :{" "}
-              <span className="bluish-grey-text">
-                {Math.round(weather.humidity)}%
-              </span>
-            </li>
-            <li>
-              Wind:
-              <span className="bluish-grey-text">
-                {" "}
-                {Math.round(weather.windSpeed * 3.6)} km/h
-              </span>
-            </li>
-            <li>
-              Today's Low: <span className="bluish-grey-text">-1°C</span>{" "}
-            </li>
-            <li>
-              Today's High: <span className="bluish-grey-text">10°C</span>
-            </li>
-          </ul>
-        </div>
+        <div className="text-center location">{weather.city}</div>
       </div>
-    );
-  } else {
-    return (
-      <div className="CurrentWeather">
-        <div className="left-float">
-          <div className="temperature-wrapper">
-            <span className="icon">
-              <CurrentWeatherIcon iconName={weather.icon} />
-            </span>{" "}
-            <span className="temperature">
-              {Math.round((weather.temperature * 9) / 5 + 32)}
-            </span>{" "}
-            <span className="unit">
-              °
-              <a href="/" onClick={showCelsius}>
-                C
-              </a>{" "}
-              | °F
+      <div>
+        <ul className="mb-0 ps-1rem">
+          <li className="text-capitalize">{weather.description}</li>
+          <li>
+            Humidity :{" "}
+            <span className="bluish-grey-text">
+              {Math.round(weather.humidity)}%
             </span>
-          </div>
-          <div className="text-center">
-            <div>
-              Feels like :{" "}
-              <span className="bluish-grey-text">
-                {Math.round((weather.realFeel * 9) / 5 + 32)} °F
-              </span>
-            </div>
-            <div className="mt-3">{weather.country}</div>
-          </div>
-          <div className="text-center location">{weather.city}</div>
-        </div>
-        <div>
-          <ul className="mb-0 ps-1rem">
-            <li className="text-capitalize">{weather.description}</li>
-            <li>
-              Humidity :{" "}
-              <span className="bluish-grey-text">
-                {Math.round(weather.humidity)}%
-              </span>
-            </li>
-            <li>
-              Wind:
-              <span className="bluish-grey-text">
-                {" "}
-                {Math.round(weather.windSpeed * 2.237)} miles/h
-              </span>
-            </li>
-            <li>
-              Today's Low: <span className="bluish-grey-text">-1°C</span>{" "}
-            </li>
-            <li>
-              Today's High: <span className="bluish-grey-text">10°C</span>
-            </li>
-          </ul>
-        </div>
+          </li>
+          <li>
+            Wind:{" "}
+            <span className="bluish-grey-text">
+              {unit === "metric"
+                ? Math.round(weather.windSpeed * 3.6) + " km/h"
+                : Math.round(weather.windSpeed * 2.237) + " miles/h"}
+            </span>
+          </li>
+          <li>
+            Today's Low: <span className="bluish-grey-text">-1°C</span>{" "}
+          </li>
+          <li>
+            Today's High: <span className="bluish-grey-text">10°C</span>
+          </li>
+        </ul>
       </div>
-    );
-  }
+    </div>
+  );
 }
