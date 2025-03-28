49001c3bf9761261c8bab265acd11a37c6bf7e53
Sai-S-Mc <sai.satti97@gmail.com>
2025-03-27 21:06:05 -0400

    Forecast API integration and created & updated ForecastDay component

--- src/components/scripts/Forecast.js
+++ src/components/scripts/Forecast.js
@@ -1,103 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Forecast.css";
-import ReactAnimatedWeather from "react-animated-weather";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import ForecastDay from "./ForecastDay";
 
-export default function Forecast() {
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col-md">
-          <div className="daily-forecast">
-            Mon
-            <br />
-            <br />
-            <ReactAnimatedWeather
-              icon={"PARTLY_CLOUDY_DAY"}
-              color={"#6090E7"}
-              size={50}
-              animate={true}
-            />
-            <br />
-            <br />
-            <div className="daily-high-low">
-              <strong>13° </strong>2°
-            </div>
-          </div>
-        </div>
-        <div className="col-md">
-          <div className="daily-forecast">
-            Mon
-            <br />
-            <br />
-            <ReactAnimatedWeather
-              icon={"PARTLY_CLOUDY_DAY"}
-              color={"#6090E7"}
-              size={50}
-              animate={true}
-            />
-            <br />
-            <br />
-            <div className="daily-high-low">
-              <strong>13° </strong>2°
-            </div>
-          </div>
-        </div>
-        <div className="col-md">
-          <div className="daily-forecast">
-            Mon
-            <br />
-            <br />
-            <ReactAnimatedWeather
-              icon={"PARTLY_CLOUDY_DAY"}
-              color={"#6090E7"}
-              size={50}
-              animate={true}
-            />
-            <br />
-            <br />
-            <div className="daily-high-low">
-              <strong>13° </strong>2°
-            </div>
-          </div>
-        </div>
-        <div className="col-md">
-          <div className="daily-forecast">
-            Mon
-            <br />
-            <br />
-            <ReactAnimatedWeather
-              icon={"PARTLY_CLOUDY_DAY"}
-              color={"#6090E7"}
-              size={50}
-              animate={true}
-            />
-            <br />
-            <br />
-            <div className="daily-high-low">
-              <strong>13° </strong>2°
-            </div>
-          </div>
-        </div>
-        <div className="col-md">
-          <div className="daily-forecast">
-            Mon
-            <br />
-            <br />
-            <ReactAnimatedWeather
-              icon={"PARTLY_CLOUDY_DAY"}
-              color={"#6090E7"}
-              size={50}
-              animate={true}
-            />
-            <br />
-            <br />
-            <div className="daily-high-low">
-              <strong>13° </strong>2°
-            </div>
-          </div>
+export default function Forecast({ city }) {
+  const [forecastData, setForecastData] = useState({ apiResponse: false });
+  
+  function handleApiResponse(response) {
+    console.log("handling api response");
+    console.log(response.data);
+    setForecastData({
+      apiResponse: true,
+      maxTemperature: Math.round(response.data.daily[0].temperature.maximum),
+      minTemperature: Math.round(response.data.daily[0].temperature.minimum),
+      timestamp: response.data.daily[0].time,
+      icon: response.data.daily[0].condition.icon,
+    });
+  }
+
+  if (forecastData.apiResponse) {
+    console.log(forecastData.apiResponse);
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <ForecastDay forecastData={forecastData} />
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    console.log(forecastData.apiResponse);
+    let apiKey = "tbfob32e017e01391b34fe15b81ad2a6";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleApiResponse);
+
+    return (
+      <div className="text-center Forecast">Loading forecast for {city}...</div>
+    );
+  }
 }
---
+++ src/components/scripts/ForecastDay.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+import WeatherIcon from "./WeatherIcon";
+
+export default function ForecastDay({ forecastData }) {
+
+  let date = new Date(forecastData.timestamp * 1000);
+  console.log(date);
+  return (
+    <div className="col-md">
+      <div className="daily-forecast">
+        Mon
+        <br />
+        <br />
+        <WeatherIcon iconName={forecastData.icon} size={50} />
+        <br />
+        <br />
+        <div className="daily-high-low">
+          <strong>{forecastData.maxTemperature}° </strong>
+          {forecastData.minTemperature}°
+        </div>
+      </div>
+    </div>
+  );
+}
